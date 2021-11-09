07c4e33618efac81463af543d2ac810b4a9d6e63
Doug-Fernando <doug.fernando95@gmail.com>
2021-11-08 22:08:25 -0300

    Adiciona prints da tela e atualiza o README

--- src/api/products.js
+++ src/api/products.js
@@ -1,16 +1,7 @@
 import axios from 'axios';
 
-const URLHASH = '1904c458eea94244ae9a665e664b1fe8';
+const URLHASH = 'e43c6d15eadd405a82f1517394c42f0d';
 const URL = `https://crudcrud.com/api/${URLHASH}/products`;
-
-const createProduct = (name, quantity, price) => ({ name, quantity, price });
-
-export const createProducts = async () => {
-  axios.post(URL, createProduct('Tomato', 477, 2.25));
-  axios.post(URL, createProduct('Onion', 75, 3.6));
-  axios.post(URL, createProduct('Potato', 321, 1.25));
-  axios.post(URL, createProduct('Cheese', 125, 21.25));
-};
 
 export const addProductDB = async (data) => axios.post(URL, data).then((response) => response);
 
